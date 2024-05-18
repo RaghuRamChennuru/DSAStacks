a67c2c5e09313e56120680d2bed53c2ca96adb09
unknown <raghuram6857@gmail.com>
2024-05-18 18:31:54 +0530

    Adv

--- src/main/java/ConsecutivePairs.java
+++ src/main/java/ConsecutivePairs.java
@@ -1,3 +1,14 @@
+/*
+* You have a string, denoted as A.
+
+To transform the string, you should perform the following operation repeatedly:
+Identify the first occurrence of consecutive identical pairs of characters within the string.
+Remove this pair of identical characters from the string.
+Repeat steps 1 and 2 until there are no more consecutive identical pairs of characters.
+The final result will be the transformed string.
+*
+* */
+
 public class ConsecutivePairs
 {
     public String solve(String A)
